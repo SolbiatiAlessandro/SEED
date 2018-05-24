6ba769073de4babbb4c443d4ea044d2a91e5931e
Solbiati Alessandro <alexsolbiati@hotmail.it>
2018-05-25 05:23:45 +0800

    audio

---
+++ js/audio.js
@@ -0,0 +1,49 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var offset = 0;
+var context = new AudioContext();
+
+function playTrack(url) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+
+  var audiobuffer;
+
+  // Decode asynchronously
+  request.onload = function() {
+    
+    if (request.status == 200) {
+      
+      context.decodeAudioData(request.response, function(buffer) {
+        var source = context.createBufferSource();
+        source.buffer = buffer;
+        source.connect(context.destination);
+        console.log('context.currentTime ' + context.currentTime);
+
+        if (offset == 0) {
+          source.start();
+          offset = context.currentTime;
+        } else {
+          source.start(0,context.currentTime - offset);
+        }
+
+      }, function(e) {
+        console.log('Error decoding audio data:' + e);
+      });
+    } else {
+      console.log('Audio didn\'t load successfully; error code:' + request.statusText);
+    }
+  }
+  request.send();
+}
+
+var tracks = document.getElementsByClassName('track');
+
+for (var i = 0, len = tracks.length; i < len; i++) {
+  tracks[i].addEventListener('click', function(e){
+
+    playTrack(this.href);
+    e.preventDefault();
+  });
+}
